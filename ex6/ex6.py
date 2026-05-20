dc1364a235430afec61282fdb0ff3b2fdc677a08
behdad <behdadja@gmail.com>
2026-05-20 18:16:30 +0330

    doing ex 6

---
+++ ex6/ex6.py
@@ -0,0 +1,18 @@
+t_o_p = 10
+x = f"There are {t_o_p} types of people."
+
+print(x)
+
+hilarious = False
+
+joke_eval = "isnt that joke so funny? {}"
+
+print(joke_eval)
+print(joke_eval.format(hilarious, "ok?",123))
+
+i = "1234"
+
+j = "5678"
+
+print(i + j)
+
